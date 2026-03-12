503943bcd9cbfec14ae38f19a44b0772c81420ef
Razor-Thert <emmanuelfernandezfernandez32@gmail.com>
2026-03-12 18:52:46 -0500

    fix android mod storage
    this fixes the storage load error on android

--- src/pc/mods/mod_storage.cpp
+++ src/pc/mods/mod_storage.cpp
@@ -114,11 +114,10 @@ static bool mod_storage_check_inputs(const char *key, const char *value, char *f
     if (gLuaActiveMod == NULL) { return false; }
 
 #ifdef __ANDROID__
-    if (!key_cached(key, value)) {
+    if (value && !key_cached(key, value)) {
         cache_key(key, value);
     }
 #endif
-
     // retrieve filename
     mod_storage_get_filename(filename);
 
